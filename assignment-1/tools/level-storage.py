48920c7cefe22e95246d557a9ae2df59beff4631
e-baird <emilyiris.baird@gmail.com>
2022-09-25 11:51:36 -0600

    update random level generator to store patterns as bytes

--- assignment-1/tools/level-storage.py
+++ assignment-1/tools/level-storage.py
@@ -1,16 +1,21 @@
 import random
+import bitstring
 
-patterns = [
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-    [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1],
-    [0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0]
+# each two-byte number represents a 16-chunk pattern that can
+# be displayed on screen: when read as binary, a 1 indicates a block,
+# and a 0 indicates empty space
+hex_patterns = [
+    b"\x40\x08",
+    b"\x30\x00",
+    b"\x8c\x20",
+    b"\xc1\x91",
+    b"\x83\x42",
+    b"\x08\x24",
+    b"\x04\xc4",
+    b"\x01\x03"
 ]
 
+# assumes screen is 16 blocks tall and level is 6 full screens
 def generate_level():
     level = []
     for _ in range(96):
@@ -18,27 +23,42 @@ def generate_level():
     
     return level
 
-
+# display a level using ascii art
 def print_level(level):
     print("----------------------------------")
 
-    for v in level:
+    for i in level:
         print("|", end="")
 
-        row = patterns[v]
+        # grab the proper bytes representing this row
+        row = hex_patterns[i]
 
-        for block in row:
-            if block:
+        # convert the pattern to a string of 1s and 0s
+        bit_row = bitstring.BitArray(row)
+
+        # display ascii on screen corresponding to bits
+        for b in bit_row:
+            if b:
                 print("\u2588\u2588", end="")
             else:
                 print("  ", end="")
         
         print("|")
     print("----------------------------------")
-    
 
 
 if __name__ == "__main__":
     l = generate_level()
     print_level(l)
-    print()
+
+# # these are the same patterns as hex_patterns, but human readable
+# patterns = [
+#     [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+#     [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#     [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+#     [1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1],
+#     [1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0],
+#     [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0],
+#     [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1],
+#     [0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0]
+# ]
