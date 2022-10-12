5545ff1f8651b4e04294d237785f7a5eda1a3a72
e-baird <emilyiris.baird@gmail.com>
2022-10-12 17:28:14 -0600

    add screen dimension code to full_colour_fill

--- a1-test-files/full_colour_fill.asm
+++ a1-test-files/full_colour_fill.asm
@@ -4,12 +4,12 @@
 ; useful as a subroutine so that you don't have to reset the colour data
 ; on multiple screen redraws
 ;
-; assumes that the screen is 16*16, thus, only fills 256 spaces
-;
 ; ----------------------------------
     processor 6502
 
 ; memory location macros
+COLUMNS_ADDR = $9002
+ROWS_ADDR = $9003
 COLOR_MEM = $9600
 
 ; BASIC stub to hand over control to machine language
@@ -24,15 +24,32 @@ stubend
 
 ; start of program
 start
-    ldy     $00             ; initialize loop counter
+
+; setup for screen dimensions
+screen_dim
+    lda     #$90                    ; bit pattern 10010000, lower 6 bits = 16
+    sta     COLUMNS_ADDR            ; store in columns addr to set screen to 16 cols
+
+    lda     #$20                    ; bit pattern 00101000, bits 1to6 = 16
+    sta     ROWS_ADDR               ; store in rows addr to set screen to 16 rows
+
+    lda     #$09                    ; i don't know what i'm doing
+    sta     $9000                   ; horizontal screen centering
+
+    lda     #$20                    ; i don't know why this value works but it does
+    sta     $9001                 	; vertical screen centering
+
+
+
+    ldy     $00                     ; initialize loop counter
 
 color_fill
-    lda     $00             ; colour code for black
-    sta     COLOR_MEM,y     ; store at COLOR_MEM+y
+    lda     $00                     ; colour code for black
+    sta     COLOR_MEM,y             ; store at COLOR_MEM+y
 
-    iny                     ; increment loop counter
-    bne     color_fill      ; loop until y overflows at 256
+    iny                             ; increment loop counter
+    bne     color_fill              ; loop until y overflows at 256
     
 
-    rts                     ; return to calling code6
+    rts                             ; return to calling code6
 
