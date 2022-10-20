301cf530770a020bd1b494bd050d1646d2a48364
dylanleclair <dylan.leclair@icloud.com>
2022-10-20 12:10:13 -0600

    minor changes to improve readability

--- a2-compression-tests/runlength-decompression.asm
+++ a2-compression-tests/runlength-decompression.asm
@@ -7,7 +7,7 @@
 ;   * Compression details:
 ;       - 0x00 is the null terminator
 ;       - hi bit is the colour code (0 indicates black, 1 purple)
-;       - lo 7 bits indicate the repetitions
+;       - lo 7 bits indicate the # of repetitions
 ;
 ;   author: Emily, Sarina, Jeremy, Dylan
 ;
@@ -25,12 +25,17 @@ CENTERING_ADDR = $9001          ; stores the screen centering values
 COLUMNS_ADDR = $9002            ; stores the number of columns on screen
 ROWS_ADDR = $9003               ; stores the number of rows on screen
 
+
+; PROGRAM VARIABLES
+ENC_BYTE_VAR = $00
+ENC_BYTE_INDEX_VAR = $01
+
     processor 6502
     org $1001
     
-    dc.w stubend ; define a constant to be address @ stubend
+    dc.w stubend                ; define a constant to be address @ stubend
     dc.w 12345 
-    dc.b $9e, "4157", 0         ; jump to the end of the 'encoding' array
+    dc.b $9e, "4157", 0         ; jump to the end of the 'encoding' array (0x103d)
 stubend
     dc.w 0
 
@@ -104,22 +109,22 @@ screen_dim
 ; END SCREEN_DIM  
 
 ; DEF DECOMPRESSION
-; we assume that the byte is at location $00 on 0 page
-; and that the offset counter for the encoding array is at $01
+; we assume that the encoded (?) byte is at ENC_BYTE_VAR
+; and that the offset counter for the encoding array is at ENC_BYTE_INDEX_VAR
 
     lda     #0                  ; initialize the offset values for the 'encoding' array, and for the screen/colour memory
     tay                         ; keep one copy in y, for use as the screen/colour memory offset
-    sta     $01                 ; keep another copy on the 0-page for indexing into the 'encoding' array
+    sta     ENC_BYTE_INDEX_VAR  ; keep another copy on the 0-page for indexing into the 'encoding' array
 
     jmp     decompress_check    ; immediately jump down to the loop check for 'decompress'
 decompress
     ; setup loop counter for 'draw' loop (dealing with lo 7 bits)
     ; accumulator already has the byte loaded from 'decompress_check'
     and     #$7f                ; bitmask out the top bit, now accumulator should have the length
-    tax                         ; put this length into the loop counter for 'draw'
+    tax                         ; put this length into the loop counter for 'draw' (x register)
 
     ; setup colour conditional (dealing with hi 1 bit)
-    lda     $00                 ; get the byte back out of memory after we clobbered it with 0x7f
+    lda     ENC_BYTE_VAR        ; get the encoded byte back out of memory after we mask out MSB with 0x7f
     bmi     purple              ; check if the hi bit is 1, using 'branch on negative'
 
     ; if hi bit is 0, write black to colour memory
@@ -129,6 +134,7 @@ decompress
     ; if hi bit is 1, write purple to colour memory
 purple
     lda     #4                  ; load 04 (purple color)
+    ; jmp draw (is redundant)
 
 ; draws the proper character to the screen for 'x' loops
 draw
@@ -145,16 +151,17 @@ draw
 ; because we only exit the draw loop when x reaches 0, its value is no longer important
 ; so x is freed up for use here
 decompress_check
+    ; loop test for decompression algorithm
     ; get the offset for the 'encoding' array
-    ldx     $01                 ; grab our encoding offset from the zero-page
+    ldx     ENC_BYTE_INDEX_VAR  ; grab our encoding offset from the zero-page
 
     ; get a byte to analyze
     lda     encoding,x          ; grab the byte stored at encoding+x
-    sta     $00                 ; store the byte on zero-page for the decompress loop to access
+    sta     ENC_BYTE_VAR        ; store the byte on zero-page for the decompress loop to access
 
     ; put the 'encoding' loop counter back in memory
     inx                         ; increment the offset by 1 for next time
-    stx     $01                 ; store the encoding offset back on the zero-page
+    stx     ENC_BYTE_INDEX_VAR  ; store the encoding offset back on the zero-page
 
     cmp     #0                  ; comparing accumulator to 0x00
     bne     decompress          ; if not a null byte, go back to top of loop 
