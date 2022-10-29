fcf1aac72699d83d9bbeaff80d573c9ca3cb09cf
e-baird <emilyiris.baird@gmail.com>
2022-10-28 19:39:32 -0600

    tidy up decrunch wrapper

--- a2-compression-tests/exomizer-compression-test/decrunch-wrapper.asm
+++ a2-compression-tests/exomizer-compression-test/decrunch-wrapper.asm
@@ -1,7 +1,9 @@
 ; -----------------------------------------------------------------------------
 ;
-;   Thing that calls another thing
-;   * tries very hard to get the exomizer decruncher to decrunch
+;   Exomizer decrunch wrapper
+;   * sets up our screen dimensions and colour
+;   * places compressed data in memory for Exomizer's decruncher to find
+;   * jsr's to Exomizer decruncher
 ;
 ;   author: Emily
 ;
@@ -36,13 +38,13 @@ WORKING_SCREEN_HI = $03         ; same as above, hi byte
     
     dc.w stubend                ; define a constant to be address @ stubend
     dc.w 12345 
-    dc.b $9e, "4216", 0         ; jump to the end of the 'encoding' array (0x103d)
+    dc.b $9e, "4216", 0         ; jump to the start of machine code
 stubend
     dc.w 0
 
 ; -----------------------------------------------------------------------------
 ; PACKED DATA
-; - this is the compressed data from exomizer
+; * this is the compressed data from the Exomizer compression cli
 ; -----------------------------------------------------------------------------
 packed_char_data 
     incbin "char-crunch.x"
@@ -52,13 +54,10 @@ packed_color_data
     incbin "color-crunch.x"
 packed_color_data_end
 
-; packed_char_len = packed_char_data - packed_char_data_end
-; packed_color_len = packed_color_data - packed_color_data_end
-
 ; -----------------------------------------------------------------------------
-; DEF SCREEN_SETUP
-; - change the screen size to 16x16
-; - change background colour to black
+; SCREEN SETUP
+; * change the screen size to 16x16
+; * change border colour to black with white text
 ; -----------------------------------------------------------------------------
 screen_dim
     lda     #$90                ; bit pattern 10010000, lower 6 bits = 16
@@ -76,7 +75,8 @@ screen_dim
 
 ; -----------------------------------------------------------------------------
 ; EXOMIZER JUMP
-; - here's the point where we get on our knees and pray
+; * sets up the necessary values for Exomizer's decruncher to play nicely
+; * the setup for colour data modifies the contents of exod_get_crunched_byte
 ; -----------------------------------------------------------------------------
 exomizer_jump
     ; decompress screen data
@@ -84,17 +84,22 @@ exomizer_jump
 
     ; decompress colour data
     lda     #$78                ; lo byte of packed_color_data_end address
-    sta     _byte_lo            ; self-modifying: tell exod_crunched_byte to start reading from the
+    sta     _byte_lo            ; self-modifying: tells exod_crunched_byte to start reading from the
                                 ; color data file instead of screen data file
     jsr     exod_decrunch       ; jump to decrunch routine again
 
-    jmp     nop_loop
+    jmp     nop_loop            ; prevent return to BASIC prompt
 
 ; -----------------------------------------------------------------------------
 ; SUBROUTINE: EXOD_GET_CRUNCHED_BYTE
-; this is a subroutine that the decruncher will jsr to in order to grab the
-; next byte of data it needs to read. loads from packed_data_end because 
-; exomizer decompresses backward, from end to beginning
+; * this is a subroutine that the decruncher will jsr to in order to grab a
+;   byte of compressed data and decompress it 
+; * starts at the end of the packed data because exomizer decompresses backward, 
+;   from end to beginning
+; * self-modifying: each call to this subroutine will decrement the memory address 
+;   that the LDA instruction reads from
+; * found online. This seems to be the standard implementation of exod_get_crunched_bytes
+;   so it turns up in lots of example code.
 ; -----------------------------------------------------------------------------
 exod_get_crunched_byte:
     lda     _byte_lo
@@ -107,6 +112,9 @@ _byte_hi = * + 2
     lda     packed_char_data_end
     rts
 
+; -----------------------------------------------------------------------------
+; The ol' nop loop, prevents us from returning to BASIC prompt
+; -----------------------------------------------------------------------------
 nop_loop
     jmp     nop_loop
 
