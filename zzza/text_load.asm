b0870385d763d7deb44c33890d13caf20c0be299
Jeremy Stuart <stuartj@ucalgary.ca>
2022-12-07 09:59:28 -0700

    optimize text_load code

--- zzza/text_load.asm
+++ zzza/text_load.asm
@@ -6,13 +6,13 @@
 ; -----------------------------------------------------------------------------
 
 level_display
-    lda     CURRENT_LEVEL               ; load the current level into A register
-    clc                                 ; clear carry
-    adc     #1                          ; we index levels from 0 (BECAUSE WE LIVE IN A SOCIETY!), so add 1
+    ldx     CURRENT_LEVEL               ; load the current level into Y register
+    inx                                 ; increment it b/c we index levels at 0 (not 1 - BECAUSE WE LIVE IN A SOCIETY!)
+    txa                                 ; put x back into A
     cmp     #10                         ; check if the 10s digit needs to be set
     bmi     single_digit                ; if value is minus, only the 1s digit is set
 
-    ldx     #49                         ; char value for 1
+    lda     #49                         ; char value for 1
     sta     $1e97                       ; store in the 10s place
     sec                                 ; set carry for subtraction
     sbc     #10                         ; subtract 10 to setup for dealing with 1s position
