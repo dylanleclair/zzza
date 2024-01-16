20c79d7efed691ca62ef61ea75e3010573d3f2d6
e-baird <emilyiris.baird@gmail.com>
2024-01-15 19:57:59 -0500

    cleanup comments in charset

--- zzza/custom_charset.asm
+++ zzza/custom_charset.asm
@@ -1,47 +1,5 @@
 ; actually starts at character 2 + 8 = 10
 
-; -----------------------------------------------------------------------------
-; High resolution graphics buffer
-;
-;   * occupies characters 18 to 26
-;   * is a buffer of 9 characters, with EVA at the center by default
-;       * is 9 characters so all tiles adjacent to EVA are included
-;       * this means EVA can go anywhere in the buffer
-;       * buffer can go anywhere on screen!
-;
-;   Mapping of character to buffer (i,j):
-;
-;   -------------------
-;   | 0,0 | 0,1 | 0,2 |
-;   -------------------
-;   | 1,0 | 1,1 | 1,2 |
-;   -------------------
-;   | 2,0 | 2,1 | 2,2 |
-;   -------------------
-;
-;   To keep scrolling logic intact, and since it looked much better than re-drawing the whole screen, we actually 
-;   back up the section of screen memory where high-res graphics are being displayed. We restore it to the screen 
-;   just before calling the scrolling code (which updates it), then save it again before it is masked onto the 
-;   high-resolution graphics. 
-;
-;   It's also very important that any time EVA's position is updated that 1. we put the backed-up data back onto the screen
-;   before updating her position and 2. after her position changes, we backup the data again so so it (and any screen data) 
-;   doesn't get lost / clobbered.
-;
-;   Finally, the high resolution graphics characters are drawn to screen based on EVA's position.
-;
-;   The buffer is always centered around EVAs position, and a number of "shifts" either left, right, up or down. 
-;   Each "shift" function shifts the ENTIRE high-res framebuffer by one bit in the appropriate direction. 
-;   
-;   For example, to draw EVA 3/4 of a block down and 3/4 of a block to the right, you must call the shift_down and shift_left functions each 6 times (order doesn't matter).
-;
-;   After EVA reaches the edge of the buffer, it should be reset (clear out all data, put EVA back in the center of the buffer), with her position (X_COOR, Y_COOR) updated
-;   s.t. her new position is where had effectively moved to. For example, if you shift 8 down, you should revert the graphics buffer & increment Y_COOR. 
-;   
-;   This gives the illusion that EVA has moved from one block to the next, while preparing the buffer to move once more. 
-;
-; -----------------------------------------------------------------------------
-
 empty_block ;2
     dc.b #%00000000
     dc.b #%00000000
@@ -131,10 +89,27 @@ eva_front ;10
     dc.b #%11111111
     dc.b #%10000010
 
+; -----------------------------------------------------------------------------
+; High resolution graphics buffer
+;
+;   * occupies characters 11 to 13
+;   * is a buffer of 3 characters, with EVA at the center by default
+;
+;   Mapping of character to buffer (i,j):
+;
+;   -------
+;   | 0,0 |
+;   -------
+;   | 0,1 | <---- Eva's default location
+;   -------
+;   | 0,2 |
+;   -------
+;
+; -----------------------------------------------------------------------------
+
 hi_res_0_0 ; 11
     dc.b #0,#0,#0,#0,#0,#0,#0,#0    ; square above eva in hi-res
 hi_res_0_1 ; 12
-    ; dc.b #0,#0,#0,#0,#0,#0,#0,#0    ; eva's square in hi-res
     dc.b #%00111100             ; start with the "character" in the middle of the buffer
 	dc.b #%01000010
 	dc.b #%10100101
@@ -147,7 +122,10 @@ hi_res_0_2  ; 13
     dc.b #0,#0,#0,#0,#0,#0,#0,#0    ; square below eva in hi-res
 
 ; -----------------------------------------------------------------------------
-; hi-res below this point is no longer needed
+
+
+; -----------------------------------------------------------------------------
+; JUNK!!!!! GET RID OF IT!!!
 ; -----------------------------------------------------------------------------
 
 hi_res_1_0  ; 14
@@ -171,6 +149,10 @@ hi_res_2_1 ; 18
 hi_res_2_2 ; 19
     dc.b #0,#0,#0,#0,#0,#0,#0,#0
 
+; -----------------------------------------------------------------------------
+; END OF JUNK
+; -----------------------------------------------------------------------------
+
 eva_left_sprite ; 20
     dc.b #%00101010
     dc.b #%00111010
@@ -180,7 +162,7 @@ eva_left_sprite ; 20
     dc.b #%00010001
     dc.b #%11111111
     dc.b #%10001000
-eva_right_sprite ; 21
+eva_right_sprite ; 15
     dc.b #%10101000
     dc.b #%10101100
     dc.b #%00101100
@@ -190,7 +172,7 @@ eva_right_sprite ; 21
     dc.b #%11111111
     dc.b #%00100010
 
-falling_block: ; 22
+falling_block: ; 16
     dc.b #%10101010
     dc.b #%10101010
     dc.b #%10101010
@@ -199,7 +181,7 @@ falling_block: ; 22
     dc.b #%10101010
     dc.b #%10101010
     dc.b #%10101010
-secret_block: ; 23
+secret_block: ; 17
     dc.b #%11111111
     dc.b #%11111111
     dc.b #%11111111
@@ -208,7 +190,7 @@ secret_block: ; 23
     dc.b #%11111111
     dc.b #%11111111
     dc.b #%11111111
-p_zzza; 24
+p_zzza; 18
     dc.b #%11111111
     dc.b #%11100111
     dc.b #%11010001
@@ -217,7 +199,7 @@ p_zzza; 24
     dc.b #%10001001
     dc.b #%01000111
     dc.b #%00111111
-door: ; 25
+door: ; 19
     dc.b #%11111111
     dc.b #%10000001
     dc.b #%10000001
